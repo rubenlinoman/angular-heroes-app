a12114ea7f77e5f7d226bdf2dc1b3332a8cc4486
Rubén Lino <rubenlinomandado@gmail.com>
2023-09-13 20:05:17 +0200

    Fin sección 15 (CRUD)

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -40,8 +40,8 @@ export class HeroesService {
   delteHeroById(id: string): Observable<boolean> {
     return this.http.delete(`${this.baseUrl}/heroes/${id}`)
       .pipe(
-        catchError(err => of(false)),
-        map(resp => true)
+        map(resp => true),
+        catchError(err => of(false))
       );
   }
 }
